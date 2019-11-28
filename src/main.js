c1005af309669677c7d991ecc8ef2dab57aad4c1
panxuesen <13552198363@163.com>
2019-11-28 14:05:18 +0800

    -

--- src/main.js
+++ src/main.js
@@ -6,14 +6,13 @@ import './assets/css/base.scss'
 import REM from './assets/js/rem'
 import VConsole from 'vconsole'
 import 'mint-ui/lib/style.css'
-import { Header, Button, Popup } from 'mint-ui'
+import { Header, Button } from 'mint-ui'
 
 new VConsole()
 Vue.config.productionTip = false
 
 Vue.component(Header.name, Header)
 Vue.component(Button.name, Button)
-Vue.component(Popup.name, Popup)
 
 Vue.use(REM, 750, 750)
 
